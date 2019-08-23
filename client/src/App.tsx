3c48f61fc4554d00614c84dda704ffe4d5781886
Dmitrii Kovalev <dmitrii.kovalev@netcracker.com>
2019-08-23 15:51:13 +0400

    fixed typescript error + added tslint config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,141 +1,132 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios'
 // import { Entries } from './components/Entries';
 
 type Data = {
-  id: number,
-  message: string,
+    id: number,
+    _id: string,
+    message: string,
 }
 
 interface State {
-  data: Data,
-  id: number,
-  message: string,
-  intervalIsSet?: NodeJS.Timeout,
-  idToDelete: number,
-  idToUpdate: number,
-  objectToUpdate: Object
+    data: Data,
+    id: number,
+    message: string,
+    intervalIsSet?: NodeJS.Timeout,
+    idToDelete: number,
+    idToUpdate: number,
+    objectToUpdate: Object
 }
 
-interface Props { }
+interface Props {
+}
 
 class App extends Component<Props, State> {
 
-  state: State;
+    state: State;
 
-  constructor(props: Props) {
-    super(props);
+    constructor(props: Props) {
+        super(props);
 
-    this.state = {
-      data: {
-        id: 0,
-        message: ''
-      },
-      id: 0,
-      message: '',
-      intervalIsSet: setInterval(this.getDataFromDb, 1),
-      idToDelete: 0,
-      idToUpdate: 0,
-      objectToUpdate: [],
+        this.state = {
+            data: {
+                id: 0,
+                message: '',
+                _id: ''
+            },
+            id: 0,
+            message: '',
+            intervalIsSet: setInterval(this.getDataFromDb, 1000),
+            idToDelete: 0,
+            idToUpdate: 0,
+            objectToUpdate: [],
+        };
+    }
+
+    componentDidMount() {
+        this.getDataFromDb();
+    }
+
+    componentWillUnmount() {
+        if (this.state.intervalIsSet) {
+            clearInterval(this.state.intervalIsSet);
+            this.setState({
+                ...this.state,
+                intervalIsSet: undefined
+            });
+        }
+    }
+
+    getDataFromDb = () => {
+        fetch('http://localhost:3001/api/getData')
+            .then((data) => data.json())
+            .then((res) => this.setState({data: res.data}));
     };
-  }
 
-  componentDidMount() {
-    this.getDataFromDb();
-    if (!this.state.intervalIsSet) {
-      let interval = setInterval(this.getDataFromDb, 1000);
-      this.setState({
-        ...this.state,
-        intervalIsSet: interval
-      });
-    }
-  }
+    putDataToDB = (message: string) => {
+        console.log(Object.values(this.state.data));
+        // Object.assign(this.state.data, { id: })
+        // let currentIds = this.state.data.map((data: Data) => data.id);
+        let idToBeAdded = 0;
+        // while (currentIds.includes(idToBeAdded)) {
+        //   ++idToBeAdded;
+        // }
 
-  componentWillUnmount() {
-    if (this.state.intervalIsSet) {
-      clearInterval(this.state.intervalIsSet);
-      this.setState({
-        ...this.state,
-        intervalIsSet: undefined
-      });
-    }
-  }
+        axios.post('http://localhost:3001/api/putData', {
+            id: idToBeAdded,
+            message: message,
+        });
+    };
 
-  getDataFromDb = () => {
-    fetch('http://localhost:3001/api/getData')
-      .then((data) => data.json())
-      .then((res) => this.setState({ data: res.data }));
-  };
+    deleteFromDB = (idTodelete: number) => {
+        // parseInt(idTodelete);
+        let objIdToDelete = null;
+        // this.state.data.forEach((dat: Data) => {
+        //   if (dat.id === idTodelete) {
+        //     objIdToDelete = dat._id;
+        //   }
+        // });
 
-  putDataToDB = (message: string) => {
-    console.log(Object.values(this.state.data));
-    // Object.assign(this.state.data, { id: })
-    // let currentIds = this.state.data.map((data: Data) => data.id);
-    let idToBeAdded = 0;
-    // while (currentIds.includes(idToBeAdded)) {
-    //   ++idToBeAdded;
-    // }
+        axios.delete('http://localhost:3001/api/deleteData', {
+            data: {
+                id: objIdToDelete,
+            },
+        });
+    };
 
-    axios.post('http://localhost:3001/api/putData', {
-      id: idToBeAdded,
-      message: message,
-    });
-  };
+    updateDB = (idToUpdate: number, updateToApply: string) => {
+        let objIdToUpdate = null;
+        // parseInt(idToUpdate);
+        // this.state.data.forEach((dat: Data) => {
+        //   if (dat.id === idToUpdate) {
+        //     objIdToUpdate = dat._id;
+        //   }
+        // });
 
-  deleteFromDB = (idTodelete: number) => {
-    // parseInt(idTodelete);
-    let objIdToDelete = null;
-    // this.state.data.forEach((dat: Data) => {
-    //   if (dat.id === idTodelete) {
-    //     objIdToDelete = dat._id;
-    //   }
-    // });
+        axios.post('http://localhost:3001/api/updateData', {
+            id: objIdToUpdate,
+            update: {message: updateToApply},
+        });
+    };
 
-    axios.delete('http://localhost:3001/api/deleteData', {
-      data: {
-        id: objIdToDelete,
-      },
-    });
-  };
+    render() {
+        const {data} = this.state;
 
-  updateDB = (idToUpdate: number, updateToApply: string) => {
-    let objIdToUpdate = null;
-    // parseInt(idToUpdate);
-    // this.state.data.forEach((dat: Data) => {
-    //   if (dat.id === idToUpdate) {
-    //     objIdToUpdate = dat._id;
-    //   }
-    // });
-
-    axios.post('http://localhost:3001/api/updateData', {
-      id: objIdToUpdate,
-      update: { message: updateToApply },
-    });
-  };
-
-  render() {
-    const { data } = this.state;
-    // console.log("data inside render method");
-    // console.log(data);    
-    Object.values(data).map((element) => {
-      console.log(element);
-    });
-
-    return (
-      <div>
-        <ul>
-          {
-            !data
-              ? 'NO DB ENTRIES'
-              : Object.values(data).map((element) => (
-                <li style={ { padding: '10px' } } key={ data.message }>
-                  <span style={ { color: 'gray' } }> id: </span> { element.id } <br />
-                  <span style={ { color: 'gray' } }></span> { element.message }
-                </li>
-              ))
-          }
-        </ul>
-        {/* <div style={ { padding: '10px' } }>
+        return (
+            <div>
+                <ul>
+                    {
+                        !data
+                            ? 'NO DB ENTRIES'
+                            : Object.values(data).map((element: any) => (
+                                <li style={{padding: '10px'}} key={data._id}>
+                                    <span style={{color: 'gray'}}> id: </span> {element.id} <br/>
+                                    <span style={{color: 'gray'}}> message: </span> {element.message}
+                                </li>
+                            ))
+                    }
+                </ul>
+                {/* <div style={ { padding: '10px' } }>
           <input
             type="text"
             onChange={ (e) => this.setState({ message: e.target.value }) }
@@ -169,16 +160,16 @@ class App extends Component<Props, State> {
             onChange={ (e) => this.setState({ updateToApply: e.target.value }) }
             placeholder="put new value of the item here"
           /> */}
-        {/* <button
+                {/* <button
             onClick={ () =>
               this.updateDB(this.state.idToUpdate, this.state.updateToApply)
             }>
             UPDATE
           </button> */}
-        {/* </div> */ }
-      </div>
-    )
-  }
+                {/* </div> */}
+            </div>
+        )
+    }
 }
 
 export default App;
