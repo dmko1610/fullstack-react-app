b8e9787e72733e3e54fc809fe9c63508c43bda99
Dmitrii Kovalev <dmitrii.kovalev@netcracker.com>
2019-08-26 15:23:07 +0400

    added actions

--- client/src/store/app/app.actions.ts
+++ client/src/store/app/app.actions.ts
@@ -1,5 +1,5 @@
-import { ActionsUnion, createAction } from '../action-helpers';
-import {create} from "domain";
+import {ActionsUnion, createAction} from '../action-helpers';
+import {Dispatch} from "redux";
 
 export const CLEAR_INTERVAL = '[app] CLEAR_INTERVAL';
 export const SET_DATA_FROM_DB = '[app] SET_DATA_FROM_DB';
@@ -12,12 +12,36 @@ export const Actions = {
     setDataFromDb: () => createAction(SET_DATA_FROM_DB),
     addDataToDb: () => createAction(ADD_DATA_TO_DB),
     deleteDataFromDb: () => createAction(DELETE_DATA_FROM_DB),
-    updateDbData: () =>  createAction(UPDATE_DB_DATA)
-}
+    updateDbData: () => createAction(UPDATE_DB_DATA)
+};
 
 export const Thunks = {
-
-}
+    clearInterval: () => {
+        return (dispatch: Dispatch) => {
+            dispatch(Actions.clearInterval());
+        };
+    },
+    setDataFromDb: () => {
+        return (dispatch: Dispatch) => {
+            dispatch(Actions.setDataFromDb());
+        };
+    },
+    addDataToDb: () => {
+        return (dispatch: Dispatch) => {
+            dispatch(Actions.addDataToDb());
+        };
+    },
+    deleteDataFromDb: () => {
+        return (dispatch: Dispatch) => {
+            dispatch(Actions.deleteDataFromDb());
+        };
+    },
+    updateDbData: () => {
+        return (dispatch: Dispatch) => {
+            dispatch(Actions.updateDbData());
+        };
+    }
+};
 
 export type Actions = ActionsUnion<typeof Actions>;
 export type Thunks = ActionsUnion<typeof Thunks>;
