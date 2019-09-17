a3a00490726e95ba57114ad9821c7f03429776b6
Dmitrii Kovalev <dmitrii.kovalev@netcracker.com>
2019-09-17 14:07:14 +0400

    added Button to the Order Summary component + added total price

--- client/src/components/Burger/OrderSummary/OrderSummary.tsx
+++ client/src/components/Burger/OrderSummary/OrderSummary.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Auxiliary from '../../../hoc/Auxiliary'
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props: any) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -15,7 +16,12 @@ const orderSummary = (props: any) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout?</p>
+            <Button btnType="Danger"
+                    clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success"
+                    clicked={props.purchaseContinued}>CONTINUE</Button>
         </Auxiliary>
     )
 };
