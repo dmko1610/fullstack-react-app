2ab29fe1a42d25f9ad3a286cd8b2598b2dbcfffa
Dmitrii Kovalev <dmitrii.kovalev@netcracker.com>
2019-09-17 16:35:36 +0400

    added Navigation Bar

--- client/src/components/Navigation/Toolbar/Toolbar.tsx
+++ client/src/components/Navigation/Toolbar/Toolbar.tsx
@@ -2,14 +2,14 @@ import React from "react";
 // @ts-ignore
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props: any) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
-        <div>LOGO</div>
         <nav>
-            ...
+            <NavigationItems/>
         </nav>
     </header>
 );
