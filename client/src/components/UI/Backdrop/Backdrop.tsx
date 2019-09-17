180177bf80dfdfbfe2e82bd5885f6754e94c5611
Dmitrii Kovalev <dmitrii.kovalev@netcracker.com>
2019-09-17 12:39:21 +0400

    added backdrop component + fixed ts style error

---
+++ client/src/components/UI/Backdrop/Backdrop.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// @ts-ignore
+import classes from './Backdrop.css';
+
+const backdrop = (props: any) => (
+    props.show
+        ? <div
+            className={classes.Backdrop}
+            onClick={props.clicked}
+        />
+        : null
+);
+
+export default backdrop;
--- client/src/components/UI/Modal/Modal.tsx
+++ client/src/components/UI/Modal/Modal.tsx
@@ -1,23 +1,31 @@
 import React from "react";
 // @ts-ignore
 import classes from './Modal.css';
+import Auxiliary from '../../../hoc/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
 
 const styles = {
     show: {
         transform: 'translateY(0)',
-        opacity: '1'
-    },
+        opacity: 1
+    } as React.CSSProperties,
     hide: {
         transform: 'translateY(100vh)',
-        opacity: '0'
-    }
+        opacity: 0
+    } as React.CSSProperties
 };
 
 const modal = (props: any) => (
-    <div
-        className={classes.Modal}
-        >
-        {props.children}
-    </div>
+    <Auxiliary>
+        <Backdrop
+            show={props.show}
+            clicked={props.modalClosed}
+        />
+        <div
+            className={classes.Modal}
+            style={props.show ? styles.show : styles.hide}>
+            {props.children}
+        </div>
+    </Auxiliary>
 );
 export default modal;
